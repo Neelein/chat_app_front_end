65b84454a210ccc7df3322d2173170354af2d771
Neelein <shakya12210305@gmail.com>
2024-03-05 01:21:54 +0800

    first_commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use yew::prelude::*;
+use stylist::yew::styled_component;
+
+
+#[styled_component]
+fn App() -> Html{
+html!{
+        <div>{"test"}</div>
+    }
+}
+
+
+fn main(){
+    // yew::Renderer::<App>::new().render();
+}
+
+
